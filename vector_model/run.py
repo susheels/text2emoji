e645bfaaf18d6df5a3626080f33e18429ad45d55
susheel suresh <susheel.suresh@gmail.com>
2017-08-02 15:45:35 +0530

    updated getEmoji() function

--- vector_model/run.py
+++ vector_model/run.py
@@ -9,26 +9,33 @@ PATH_word2vec = "/home/susuresh/Downloads/GoogleNews-vectors-negative300.bin.gz"
 # load all emoji vectors from emoji2vec
 e2v = gsm.KeyedVectors.load_word2vec_format(PATH_emoji2vec, binary=True)
 
-# load all word vectors from word2vec 
+# load all word vectors from word2vec
 w2v = gsm.KeyedVectors.load_word2vec_format(PATH_word2vec, binary=True)
 
 print("Loading vectors done")
 # load sentence
-sentence = "9 perfect last minute summer trip ideas"
 
-sentence_words = sentence.split(" ")
-wvecs = []
-for word in sentence_words:
-	if word in w2v.vocab:
-		vector = w2v[word]
-		wvecs.append(vector)
+def getEmoji(sentence):
+	sentence_words = sentence.split(" ")
+	wvecs = []
+	for word in sentence_words:
+		if word in w2v.vocab:
+			vector = w2v[word]
+			wvecs.append(vector)
 
-pred_emoji_list = []
-for wvec in wvecs:
-	pred_emoji_list.append(e2v.similar_by_vector(wvec,topn=2))
+	pred_emoji_list = []
+	for wvec in wvecs:
+		pred_emoji_list.append(e2v.similar_by_vector(wvec,topn=2))
 
-with open('pred_emoji_list.pkl', 'wb') as f:
-	pickle.dump(pred_emoji_list, f)
+	return pred_emoji_list
 
-with open("file1.txt", "w",encoding='utf-8') as output:
-    output.write(str(pred_emoji_list))
+sentences = ["9 perfect last minute summer trip ideas",
+			"Shop shoes and clothes at PUMA with your reward points & get 50% value back",
+			"Save up to 15% on dining with Citi cards at these new premium restaurants"]
+pel = []
+for sentence in sentences:
+	print(sentence)
+	pel.append(getEmoji(sentence))
+
+with open("pred_emojis.txt", "w",encoding='utf-8') as output:
+    output.write(str(pel))
